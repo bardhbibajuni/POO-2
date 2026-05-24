20ad65a684cb801694a7f0ca48a72d774fdcce3a
bardhbibajuni <bardh.bibaj@student.uni-pr.edu>
2026-05-25 00:23:38 +0200

    Add fields to Appointment model

--- src/com/hairsalon/model/Appointment.java
+++ src/com/hairsalon/model/Appointment.java
@@ -1,3 +1,10 @@
 package com.hairsalon.model;
 public class Appointment {
+    private int id;
+    private String clientName;
+    private String serviceName;
+    private String stylistName;
+    private String date;
+    private String time;
+    private String status;
 }
