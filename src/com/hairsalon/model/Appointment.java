2d736eccc25959d046051ef6d49028c09c06db71
bardhbibajuni <bardh.bibaj@student.uni-pr.edu>
2026-05-25 00:23:40 +0200

    Add Appointment constructor

--- src/com/hairsalon/model/Appointment.java
+++ src/com/hairsalon/model/Appointment.java
@@ -7,4 +7,14 @@ public class Appointment {
     private String date;
     private String time;
     private String status;
+    public Appointment(int id, String clientName, String serviceName, String stylistName,
+                       String date, String time, String status) {
+        this.id = id;
+        this.clientName = clientName;
+        this.serviceName = serviceName;
+        this.stylistName = stylistName;
+        this.date = date;
+        this.time = time;
+        this.status = status;
+    }
 }
