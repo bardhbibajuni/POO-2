fbb6dddf3d8a4932ccedb64bce46d25c1a4a97da
bardhbibajuni <bardh.bibaj@student.uni-pr.edu>
2026-05-25 00:23:56 +0200

    Add ClientController class skeleton

---
+++ src/com/hairsalon/controller/ClientController.java
@@ -0,0 +1,16 @@
+package com.hairsalon.controller;
+
+import com.hairsalon.database.DatabaseConnection;
+import com.hairsalon.model.Client;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class ClientController {
+}
