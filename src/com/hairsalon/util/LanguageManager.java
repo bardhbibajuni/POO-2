7ddd2efb5ac91442bbd7be3e57f3d372e37013c9
Jona <jonamehmeti4@gmail.com>
2026-05-26 11:18:20 +0200

    Language Manager

--- src/com/hairsalon/util/LanguageManager.java
+++ src/com/hairsalon/util/LanguageManager.java
@@ -1,4 +1,18 @@
 package com.hairsalon.util;
 
 public class LanguageManager {
+
+    private static String currentLanguage = "en";
+
+    public static String getCurrentLanguage() {
+        return currentLanguage;
+    }
+
+    public static void setLanguage(String language) {
+        currentLanguage = language;
+    }
+
+    public static boolean isAlbanian() {
+        return currentLanguage.equals("sq");
+    }
 }
