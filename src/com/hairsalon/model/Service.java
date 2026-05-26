7fef7b3eece1a233a96121b8aea5fabda22193aa
JonaElezi <jona.elezi@student.uni-pr.edu>
2026-05-26 11:28:26 +0200

    Service.java

--- src/com/hairsalon/model/Service.java
+++ src/com/hairsalon/model/Service.java
@@ -1,4 +1,31 @@
 package com.hairsalon.model;
 
 public class Service {
+    private int id;
+    private String name;
+    private double price;
+    private int duration;
+
+    public Service(int id, String name, double price, int duration) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.duration = duration;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
 }
