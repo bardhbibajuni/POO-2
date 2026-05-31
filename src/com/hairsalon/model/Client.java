e084920fbbe3fc102e8267d677b95715ae02a67c
bardhbibajuni <bardh@local>
2026-05-31 22:16:24 +0200

    Client.java

--- src/com/hairsalon/model/Client.java
+++ src/com/hairsalon/model/Client.java
@@ -1,4 +1,31 @@
 package com.hairsalon.model;
 
 public class Client {
+    private int id;
+    private String name;
+    private String phone;
+    private String email;
+
+    public Client(int id, String name, String phone, String email) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
