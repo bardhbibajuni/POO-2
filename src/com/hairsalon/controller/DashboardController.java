24dc339210c5137efa88ec27595660d515914003
KronPajazitiU <kron.pajaziti@student.uni-pr.edu>
2026-05-31 13:43:25 +0200

    Update DashboardController.java

--- src/com/hairsalon/controller/DashboardController.java
+++ src/com/hairsalon/controller/DashboardController.java
@@ -1,4 +1,15 @@
 package com.hairsalon.controller;
 
+import com.hairsalon.database.DatabaseConnection;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.chart.*;
+import javafx.scene.control.Label;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class DashboardController {
-}
+
+  
